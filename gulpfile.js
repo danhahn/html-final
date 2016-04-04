5e18e39199241369bfbcc4e091af42783f7106c8
Dan Hahn <dhahn@appnexus.com>
2016-04-04 12:53:41 -0400

    Clean up the gulp file to work better

--- gulpfile.js
+++ gulpfile.js
@@ -22,21 +22,21 @@ gulp.task('images', function() {
 	.pipe(notify({ message: 'Images Coppied Over' }));
 });
 
-gulp.task('replace', function() {
-	gulp.src(htmlSourcse)
-	.pipe(replace(/(<h[1,2,3]) id="[a-z\-]*">/g, '$1>'))
-	.pipe(replace(/(The Empire State)\s*/g, '$1'))
-	.pipe(replace(/(css".)\s*\</g, '$1\n\t<'))
-	.pipe(gulp.dest('builds'));
-});
+// gulp.task('replace', function() {
+// 	gulp.src(htmlSourcse)
+// 	.pipe(replace(/(<h[1,2,3]) id="[a-z\-]*">/g, '$1>'))
+// 	.pipe(replace(/(The Empire State)\s*/g, '$1'))
+// 	.pipe(replace(/(css".)\s*\</g, '$1\n\t<'))
+// 	.pipe(gulp.dest('builds'));
+// });
+//
+// gulp.task('format', function() {
+//   gulp.src('builds/*/*.html')
+//     .pipe(prettify({indent_size: 4}))
+//     .pipe(gulp.dest('builds'))
+// });
 
-gulp.task('format', function() {
-  gulp.src('builds/*/*.html')
-    .pipe(prettify({indent_size: 4}))
-    .pipe(gulp.dest('builds'))
-});
-
-gulp.task('less', function() {
+gulp.task('less', () => {
 	gulp.src(lessFiles)
 		.pipe(less())
 		.pipe(gulp.dest('builds/css'))
@@ -45,20 +45,27 @@ gulp.task('less', function() {
 });
 
 // Converts Markdown to HTML
-gulp.task('markdown', function () {
+gulp.task('markdown', () => {
     return gulp.src(markdownFiles)
         .pipe(markdown())
+		.pipe(prettify({indent_size: 4}))
+		.pipe(replace(/(<h[\d]) id="[\d\w\w\-]*">/g, '$1>'))
+		.pipe(replace(/([\w\.\,\)\;])[\r\n][ \t]+/g, '$1 '))
+		.pipe(replace(/(<\/\w[1,2,3,p,l,e]*>)/g, '$1\r\r'))
         .pipe(gulp.dest('./html/md/'))
         .pipe(connect.reload())
         .pipe(notify({ message: 'Markdown to HTML task complete' }));
 });
 
 // Converts Jade to HTML (jade is including markdown files)
-gulp.task('jade', ['markdown'], function() {  // ['markdown'] forces jade to wait
+gulp.task('jade', ['less'], () => {  // ['markdown'] forces jade to wait
     return gulp.src(jadeFiles)
         .pipe(jade({
             pretty: true,  // uncompressed
         }))
+		.pipe(prettify({indent_size: 4}))
+		.pipe(replace(/(<h[\d]) id="[\d\w\w\-]*">/g, '$1>'))
+		.pipe(replace(/([\w\.\,\)\;])[\r\n][ \t]+/g, '$1 '))
         .pipe(gulp.dest('./builds/'))
         .pipe(connect.reload())
         .pipe(notify({ message: 'Jade to HTML task complete' }));
@@ -68,23 +75,22 @@ gulp.task('connect', function() {
 	connect.server({
 		root: 'builds/',
 		port: 8000,
-		livereloadg: true
+		livereload: true
 	});
 });
 
-gulp.task("html", ['images'], function() {
+gulp.task("html", () => {
 	gulp.src(htmlSourcse)
 		.pipe(connect.reload())
 		.pipe(notify({ message: 'Watching HTML' }));
 });
 
 gulp.task('watch', function() {
-	gulp.watch(markdownFiles, ['markdown']);
 	gulp.watch(lessFiles, ['less']);
 	gulp.watch(jadeFiles, ['jade']);
 	gulp.watch(jadeSource, ['jade']);
-	gulp.watch(markdownFiles, ['jade'])
-	gulp.watch(htmlSourcse, ['html', 'images']);
+	gulp.watch(markdownFiles, ['jade']);
+	gulp.watch(imgSouce, ['images']);
 });
 
-gulp.task('default', ['html','less', 'jade', 'connect', 'watch']);
+gulp.task('default', ['images', 'jade', 'connect', 'watch']);
